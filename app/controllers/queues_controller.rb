2d2393244774a4c1195250ebb7f40608cd548b75
Reza Jelveh <toyb0mb@gmx.net>
2012-04-15 03:47:15 +0100

    keep settings

--- app/controllers/queues_controller.rb
+++ app/controllers/queues_controller.rb
@@ -2,6 +2,9 @@ class QueuesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+  end
+
+  def add
     # see if someone is already in the queue waiting for 
     # us to be connected
     msg = ApplicationController.messages_queue.pop
@@ -13,7 +16,8 @@ class QueuesController < ApplicationController
     # Show the user what we got
     if msg[:payload].present? && msg[:payload] != :queue_empty
       match = Match.create(:player1_id => msg[:payload].to_i, :player2_id => current_user.id)
-      redirect_to match_path(:id => match.unique_id)
+      # redirect_to match_path(:id => match.unique_id)
+      render :js => "window.location.href='#{match_path(:id => match.unique_id)}';"
     else
       # Send the message from the form's input box to the "messages"
       # queue, via the nameless exchange.  The name of the queue to
@@ -21,11 +25,9 @@ class QueuesController < ApplicationController
       ApplicationController.nameless_exchange.publish current_user.id,
         :key => "messages"
       # Notify the user that we published.
+      render :js => "$('#waiting').toggle();$('#add_to_queue').toggle();"
+
     end
-
-  end
-
-  def add
 
   end
 
