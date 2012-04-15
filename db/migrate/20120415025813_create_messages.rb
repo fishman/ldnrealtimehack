265766cba38ff2302e1e6d6bcd7640d953036136
Reza Jelveh <toyb0mb@gmx.net>
2012-04-15 04:08:26 +0100

    added mysql messages stuff

---
+++ db/migrate/20120415025813_create_messages.rb
@@ -0,0 +1,10 @@
+class CreateMessages < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.string :owner
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
